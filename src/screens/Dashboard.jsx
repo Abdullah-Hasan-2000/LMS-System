c0af442f8703c8058f04ecaeba92950c996cf9ce
Allukaparatha <ahjhrhah3@gmail.com>
2025-08-12 00:30:46 +0500

    navigation tab added

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import ResponsiveAppBar from '../components/Navbar/Navbar.jsx';
+import NavigationBar from '../components/NavigationBar/NavigationBar.jsx';
+import { Box, Paper } from '@mui/material';
 
-const Dashboard = ({handleSignOut}) => {
+
+const Dashboard = ({ handleSignOut }) => {
   return (
     <>
-      <ResponsiveAppBar logoutButton={handleSignOut}  />
-      <h1>Dashboard</h1>
-      <p>Welcome to the Dashboard!</p>     
+      <ResponsiveAppBar logoutButton={handleSignOut} />
+      <Box sx={{ display: 'flex', flexDirection: 'row', padding: 2 }}>
+        <NavigationBar />
+        <Box sx={{ marginLeft: 2 }}>
+          <Paper elevation={3} sx={{ padding: 2, width: '80vw', height: '100vh' }}>
+            <h1>Dashboard</h1>
+            <p>Welcome to the Dashboard!</p>
+          </Paper>
+        </Box>
+      </Box>
     </>
   )
 }
